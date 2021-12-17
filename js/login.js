0f4c3c58fe6b57a4499c6660d504d6758a1f8f53
Abhinav <abhinavmule123@gmail.com>
2021-12-17 19:47:00 +0530

    login error msg include

--- js/login.js
+++ js/login.js
@@ -19,7 +19,16 @@ $(document).ready(function(){
                 if(response.trim() == "login success"){
                     window.location = "dashboard.html";
                 }else if(response.trim() == "login failed"){
-                    console.log(response);
+                    $("#notice-msg").html(`<div class="container p-3">
+                    <div class="alert alert-danger alert-dismissible">
+                     <button type="button" class="close" data-dismiss="alert">&times;</button>
+                     <strong>E-mail ID or Password Incorrect  !</strong>
+                    </div>
+                    </div>`);
+
+                    setTimeout(()=>{
+                        $("#notice-msg").html("");
+                    },2000);
                 }
             }
         });
